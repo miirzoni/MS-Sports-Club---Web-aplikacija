2df47963fdea5e44244faede9e9d8bc80c4a9f49
Mirza Haračić <mirza.haracic.23@size.ba>
2025-11-12 13:50:27 +0100

    Lab2

---
+++ Sanjin i Mirza/src/main/java/com/example/demo/controller/SportClassController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.SportClass;
+import com.example.demo.model.Trainer;
+import com.example.demo.repositories.SportClassRepository;
+import com.example.demo.repositories.TrainerRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/classes")
+public class SportClassController {
+
+    private final SportClassRepository sportClassRepository;
+    private final TrainerRepository trainerRepository;
+
+    public SportClassController(SportClassRepository sportClassRepository, TrainerRepository trainerRepository) {
+        this.sportClassRepository = sportClassRepository;
+        this.trainerRepository = trainerRepository;
+    }
+
+    @GetMapping
+    public String listClasses(Model model) {
+        model.addAttribute("classes", sportClassRepository.findAll());
+        model.addAttribute("trainers", trainerRepository.findAll());
+        model.addAttribute("sportClass", new SportClass());
+        return "classes";
+    }
+
+    @PostMapping("/add")
+    public String addClass(@ModelAttribute SportClass sportClass) {
+        sportClassRepository.save(sportClass);
+        return "redirect:/classes";
+    }
+}
